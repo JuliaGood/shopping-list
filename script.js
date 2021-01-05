120671973845388976a92454b95d3bac9fa0d7ef
JuliaGood <gudak.yuliya@gmail.com>
2021-01-05 14:24:49 +0200

    DRY - made code refactoring

--- script.js
+++ script.js
@@ -2,21 +2,25 @@ const input = document.getElementById('userInput');
 const btn = document.getElementById('enterBtn');
 const ul = document.getElementById('itemList');
 
-btn.addEventListener('click', function() {
-    if(input.value.length > 0) {
-        const li = document.createElement('li');
+const inputLength = () => {
+    return input.value.length;
+}
+
+const createListElement = () => {
+    const li = document.createElement('li');
         li.appendChild(document.createTextNode(input.value));
         ul.appendChild(li);
         input.value = '';
+}
+
+btn.addEventListener('click', function() {
+    if(inputLength() > 0) {
+        createListElement();
     }
 });
 
 input.addEventListener('keypress', function(event) {
-    if(input.value.length > 0 && event.code === "Enter" ) {
-        //Deprecated: .which = .keyCode = .charCode === 13
-        const li = document.createElement('li');
-        li.appendChild(document.createTextNode(input.value));
-        ul.appendChild(li);
-        input.value = '';
+    if(inputLength() > 0 && event.code === "Enter" ) {
+        createListElement();
     }
-}); 
+});
