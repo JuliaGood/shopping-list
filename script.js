42dda5d2ede33dcd9382115ecbcea72ff937e60b
JuliaGood <gudak.yuliya@gmail.com>
2021-06-30 01:56:13 +0300

    getted homework tasks done

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
 const input = document.getElementById('userInput');
 const btn = document.getElementById('enterBtn');
 const ul = document.getElementById('itemList');
+const liItems = document.querySelectorAll('li');
+//or 
+//const liItems = ul.getElementsByTagName('li');
 
 const inputLength = () => {
     return input.value.length;
 }
 
 const createListElement = () => {
-    const li = document.createElement('li');
-        li.appendChild(document.createTextNode(input.value));
-        ul.appendChild(li);
-        input.value = '';
+    const cretedLi = document.createElement('li');
+    cretedLi.appendChild(document.createTextNode(input.value));
+    cretedLi.classList.add('li');
+    ul.appendChild(cretedLi);
+    input.value = '';
 }
 
 const addListAfterClick = () => {
@@ -27,3 +31,50 @@ const addListAfterKeypress = (event) => {
 
 btn.addEventListener('click', addListAfterClick);
 input.addEventListener('keypress', addListAfterKeypress);
+
+
+// Tasks for Homework
+//1. If you click on the list item, it toggles the .done  class on and off.
+function toggleDone(event) {
+    const clickedLi = event.target;
+    if (clickedLi.classList.contains('li')) {
+        return clickedLi.classList.toggle("done");
+    }
+}
+
+ul.addEventListener("click", toggleDone);
+
+//2. Add buttons next to each list item to delete the item when clicked on its 
+// corresponding delete button.
+
+function createDeleteLiBtn(event) {
+    const selectedLi = event.target;
+
+    if (selectedLi.classList.contains('li')) {
+        if (!selectedLi.querySelector('.deleteBtn')) {
+        // create btn dynamically next to list item
+            const createdBtn = document.createElement('button');
+            createdBtn.appendChild(document.createTextNode("X"));
+            createdBtn.classList.add('deleteBtn');
+            selectedLi.addEventListener('click', clickDeleteLiBtn);
+            selectedLi.appendChild(createdBtn);
+        }
+    }
+}
+
+
+ul.addEventListener("click", createDeleteLiBtn);
+
+// delete the ITEM when cliked on the deleteLiBtn
+function clickDeleteLiBtn(event) {
+    const selectedBtn = event.target;   
+
+    if (selectedBtn.classList.contains('deleteBtn')) {
+        selectedBtn.parentNode.remove(); 
+    }
+}
+
+
+//3. BONUS: When adding a new list item, it automatically adds 
+// the delete button next to it (hint: be sure to check if new items are clickable too!)
+
