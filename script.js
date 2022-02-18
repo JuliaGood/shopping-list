b90dfef471d44a852e8e976db64353a9e8cde68b
JuliaGood <gudak.yuliya@gmail.com>
2022-02-18 23:11:04 +0200

    main and major changes were made

--- script.js
+++ script.js
@@ -1,78 +1,90 @@
-const input = document.getElementById('userInput');
-const btn = document.getElementById('enterBtn');
-const ul = document.getElementById('itemList');
-const liItems = document.querySelectorAll('li');
+const input = document.getElementById('user-input');
+const btn = document.getElementById('enter-btn');
+const itemList = document.getElementById('item-list');
+const item = document.querySelectorAll('.item');
+
+const defaultTasks = ["task1", "task2"];
+const tasks = JSON.parse(localStorage.getItem('tasks') || JSON.stringify(defaultTasks));
+
+document.addEventListener('DOMContentLoaded', function () {
+    tasks.forEach((task) => {
+        createTaskElement(task);
+    });
+})
+
+const addListAfterClick = () => {
+    if (inputLength() > 0) {
+        createTaskElement();
+    }
+}
+
+const addListAfterKeypress = (event) => {
+    if (inputLength() > 0 && event.code === 'Enter') {
+        createTaskElement();
+    }
+}
 
 const inputLength = () => {
     return input.value.length;
 }
 
-const createListElement = () => {
-    const cretedLi = document.createElement('li');
-    cretedLi.appendChild(document.createTextNode(input.value));
-    cretedLi.classList.add('li');
-    ul.appendChild(cretedLi);
-    input.value = '';
-}
+const createTaskElement = (task = null) => {
+    const createdItem = document.createElement('div');
+    createdItem.classList.add('item');
 
-const addListAfterClick = () => {
-    if(inputLength() > 0) {
-        createListElement();
+    const textItem = document.createElement('div');
+    textItem.classList.add('item-text');
+
+    const btnItem = document.createElement('button');
+    btnItem.innerText = 'Delete';
+    btnItem.classList.add('delete-btn');
+    btnItem.addEventListener('click', clickDeleteItemBtn);
+
+    createdItem.append(textItem, btnItem);
+    itemList.appendChild(createdItem);
+
+    if (task) {
+        textItem.innerText = task;
+    } else {
+        textItem.innerText = input.value;
+        tasks.push(input.value);
+        localStorage.setItem('tasks', JSON.stringify(tasks));
+        input.value = '';
     }
 }
 
-const addListAfterKeypress = (event) => {
-    if(inputLength() > 0 && event.code === "Enter" ) {
-        createListElement();
-    }
-}
 
 btn.addEventListener('click', addListAfterClick);
 input.addEventListener('keypress', addListAfterKeypress);
 
 
-// Tasks for Homework
-//1. If you click on the list item, it toggles the .done  class on and off.
+//
 function toggleDone(event) {
-    const clickedLi = event.target;
-    if (clickedLi.classList.contains('li')) {
-        return clickedLi.classList.toggle("done");
+    event.stopPropagation();
+    const clickedItem = event.target;
+    if (clickedItem.classList.contains('item')) {
+        clickedItem.classList.toggle('done');
+    }
+    console.log(event.target)
+}
+itemList.addEventListener('click', toggleDone);
+
+
+
+// delete the ITEM when cliked on the deleteItemBtn
+function clickDeleteItemBtn(event) {
+    console.log('event', event);
+    const selectedBtn = event.target;
+
+    if (selectedBtn.classList.contains('delete-btn')) {
+        selectedBtn.parentNode.remove();
+        const selectedTask = selectedBtn.previousSibling.innerText;
+        console.log("task", selectedTask);
+
+        const filteredTasks = tasks.filter((task) => {
+            return task !== selectedTask;
+        })
+
+        localStorage.setItem('tasks', JSON.stringify(filteredTasks));
     }
 }
-
-ul.addEventListener("click", toggleDone);
-
-//2. Add buttons next to each list item to delete the item when clicked on its 
-// corresponding delete button.
-
-function createDeleteLiBtn(event) {
-    const selectedLi = event.target;
-
-    if (selectedLi.classList.contains('li')) {
-        if (!selectedLi.querySelector('.deleteBtn')) {
-        // create btn dynamically next to list item
-            const createdBtn = document.createElement('button');
-            createdBtn.appendChild(document.createTextNode("Delete"));
-            createdBtn.classList.add('deleteBtn');
-            selectedLi.addEventListener('click', clickDeleteLiBtn);
-            selectedLi.appendChild(createdBtn);
-        }
-    }
-}
-
-
-ul.addEventListener("click", createDeleteLiBtn);
-
-// delete the ITEM when cliked on the deleteLiBtn
-function clickDeleteLiBtn(event) {
-    const selectedBtn = event.target;   
-
-    if (selectedBtn.classList.contains('deleteBtn')) {
-        selectedBtn.parentNode.remove(); 
-    }
-}
-
-
-//3. BONUS: When adding a new list item, it automatically adds 
-// the delete button next to it (hint: be sure to check if new items are clickable too!)
-
